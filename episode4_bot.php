55bd0f3d8a5f9b74ceced7e609a23bb774c2ef78
Chris-The-Tuner <Chris-The-Tuner@users.noreply.github.com>
2017-11-18 15:27:38 +0100

    Create episode4_bot.php

---
+++ episode4_bot.php
@@ -0,0 +1,26 @@
+<?php
+$json_raw = file_get_contents("php://input");
+
+$handle = fopen("bot.log","a+");
+fwrite($handle,$json_raw . "\n\n");
+fclose($handle);
+
+$json_out = json_decode($json_raw);
+
+include 'botid.php';
+include 'send_message.php';
+include 'send_photo.php';
+
+$command_arr = explode(" ",$json_out->message->text);
+
+if($command_arr[0] == "/start")
+{
+ sendMessage($bot_id,$json_out->message->chat->id,"Willkommen auf YouTube !");
+}
+
+if($command_arr[0] == "/bild")
+{
+ sendMessage($bot_id,$json_out->message->chat->id,"Okay, geb mir nen Moment !");
+ sendPhoto($bot_id,$json_out->message->chat->id,"http://ytdev.chris-the-tuner.de/cutechickwithhairypussy.jpg");
+}
+?>
